8b8b19fee2509d4e0f26c7116fd5d8ce79154721
Soumia Sari <soumia.sari@hotmail.com>
2014-12-07 15:28:43 +0100

    placer les boite de saisie et d'emplacement

--- ExtraInfo.java
+++ ExtraInfo.java
@@ -1,4 +1,7 @@
+package ExtraInfoProgDepart;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,6 +10,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.UnknownHostException;
+
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 public class ExtraInfo {
@@ -27,6 +32,28 @@ public class ExtraInfo {
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			in.readLine();
 			in.close();
+			
+
+			//=========================Boîte de saisie du nom de fichier contenant le résultat asprie======//		
+					JOptionPane jop = new JOptionPane();
+					JOptionPane.showInputDialog(null, "File name ", "Entree",
+							JOptionPane.QUESTION_MESSAGE);
+					
+			//==================enregistré  dans  un  emplacement selon le choix de l’utilisateur via  une boîte de dialogue==========//
+						JFileChooser filechoose = new JFileChooser();
+					// Créer un JFileChooser
+					   filechoose.setCurrentDirectory(new File(".")); 
+					   // Le répertoire source du JFileChooser est le répertoire d’où est lancé notre programme
+					   String approve = new String("Choisir l'emplacement d'enregistement du ficher aspire");
+					// Le bouton pour valider l’enregistrement portera la mention ENREGSITRER
+					   int resultatEnregistrer = filechoose.showDialog(filechoose,approve);
+					   // Pour afficher le JFileChooser…
+					   if (resultatEnregistrer == JFileChooser.APPROVE_OPTION)
+						  // Si l’utilisateur clique sur le bouton ENREGSITRER
+					   { String monFichier= new String(filechoose.getSelectedFile().toString());
+					// Récupérer le nom du fichier qu’il a spécifié
+					System.out.print("\n"+monFichier);
+					      } 
 		}
 
 		//====================Erreur probleme connexion==============//
