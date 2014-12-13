186c9389fb4b7d74c2bef5b992cbb8212a5bca75
sarah <sarah.sanfilippo@hotmail.fr>
2014-12-13 08:38:51 +0100

    Traite le URL inexistante et la bar de progression

--- ExtraInfo.java
+++ ExtraInfo.java
@@ -1,24 +1,24 @@
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
-import javax.swing.JOptionPane;
+package ExtraInfoProgDepart;
 
-public class ExtraInfo {
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+
+/*import java.util.regex.*; expression reg*/
+
+public class ExtraInfo implements Runnable {
 	private static PrintWriter ecrire;
 	private static PrintWriter ecrire2;
 
 	public static void main(String[] args) throws IOException {
+
 		char c;
-		String urlstart;
+		String urlstart = null;
 		ecrire = new PrintWriter("subst");
 
 		try {
+			// =====================Creation boite de Saisi URL=========================//
 			urlstart = JOptionPane.showInputDialog(null, "URL", "Entree",JOptionPane.QUESTION_MESSAGE);
 			URL url = new URL(urlstart);
 
@@ -26,58 +26,118 @@ public class ExtraInfo {
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			in.readLine();
 			in.close();
+
+			// =========================Boîte de saisie du nom de fichier contenant le résultat asprie======//
+			new JOptionPane();
+			String nomfich = JOptionPane.showInputDialog(null, "File name ","Entree", JOptionPane.QUESTION_MESSAGE);
+			System.out.print("\n" + nomfich);
+			// ======enregistré dans un emplacement selon le choix de l’utilisateur via une boîte de dialogue==========//
+
+			JFileChooser filechoose = new JFileChooser();
+			// Créer un JFileChooser
+			filechoose.setCurrentDirectory(new File("."));
+			// Le répertoire source du JFileChooser est le répertoire d’où est lancé notre programme
+			String approve = new String(
+					"Choisir l'emplacement d'enregistement du ficher aspire");
+			// Le bouton pour valider l’enregistrement portera la mentionENREGSITRER
+
+			int resultatEnregistrer = filechoose.showDialog(filechoose, approve);filechoose.setName(nomfich);
+			// Pour afficher le JFileChooser…
+			if (resultatEnregistrer == JFileChooser.APPROVE_OPTION)
+			// Si l’utilisateur clique sur le bouton ENREGSITRER
+			{
+				String monFichier = new String(filechoose.getSelectedFile().toString());
+				// Récupérer le nom du fichier qu’il a spécifié
+				System.out.print("\n" + monFichier);
+			}
 		}
 
-		// /////Erreur probleme connexion////////
+		// ====================Erreur probleme connexion==============//
+
 		catch (UnknownHostException e) {
 			System.err.println(e);
-			JOptionPane.showMessageDialog(null,
-<<<<<<< HEAD
-					"Problème de connexion à Internet!", "Message",
-=======
-					"ProblÃ¨me de connexion Ã  Internet!", "Message",
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
-					JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null,"Problème de connexion à Internet!", "Message",JOptionPane.ERROR_MESSAGE);
 			System.exit(0);
 		}
 
-		// /////Erreur URL Vide////////////
+		// ====================Erreur URL Vide========================//
+
 		catch (MalformedURLException e) {
 			System.err.println(e);
-			JOptionPane.showMessageDialog(null, "Vous n'avez pas saisie l'URL",
-					"Message", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Vous n'avez pas saisie l'URL","Message", JOptionPane.ERROR_MESSAGE);
 			System.exit(0);
 		}
+		// =====================URL inexistante (dans ce cas l’aspiration doit
+		// continuer)================//
+		/* ou l (UnknownHostException */
+		catch (FileNotFoundException e) {
+			System.err.println(e);
+			JOptionPane.showMessageDialog(null, "Problème de connexion à URL "+ urlstart, "Message", JOptionPane.ERROR_MESSAGE);
+		}
+		// ============================Barres de progression=================================//
+
+		JPanel pan = new JPanel();
+
+		// ------------bar de progression spécifique à chaque page aspirée----------/
+		JProgressBar progressBar = new JProgressBar('A', 'B');
+		progressBar.setValue('A');
+		progressBar.setStringPainted(true);
+
+		// -------------bar de progression générale---------------------------------/
+		JProgressBar progressBar2 = new JProgressBar('A', 'Z');
+		progressBar2.setValue('A');
+		progressBar2.setStringPainted(true);
+		// -------------les labels--------------------------------------------------/
+		JLabel label2 = new JLabel("URL");/* il faut la mette dans la boucle desletter pour genere les letter devent URL*/
+		JLabel label = new JLabel("En cours...");
+
+		// -------------LaFenantre------------------------------------------------/
+		JFrame frame = new JFrame("ProgressBarDemo");
+		frame.setTitle("Aspiration...");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+
+		// ------------ Ajout des composante il faut qu'il soit dans cette ordre a cause de la fontion .pack()----/
+		pan.add(progressBar);
+		pan.add(label2);
+		pan.add(progressBar2);
+		pan.add(label);
+		// ------pour bien l'ordonne-----/
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new BorderLayout());
+		panel1.add(pan, BorderLayout.NORTH);
+		panel1.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		frame.setContentPane(panel1);
+
+		frame.setVisible(true);
+
+		// progressBar.setValue(d);
 
 		for (c = 'A'; c <= 'Z'; c++) {
-
-			// URL url = new URL("http://www.vidal.fr/Sommaires/Substances-C.htm");
+			progressBar2.setValue(c);
+			label.setText("En cours ...");
+			frame.pack();
+			// URL url = new
+			// URL("http://www.vidal.fr/Sommaires/Substances-C.htm");
 			// Ou bien...
 			urlstart = "http://www.vidal.fr/Sommaires/Substances-" + c + ".htm";
 
 			// Ou bien PrintWriter ecrire =new PrintWriter (new FileWriter("subst.dic")) ;
-<<<<<<< HEAD
-			//FileWriter n'accepte pas un 2ème argument pour spécifier l'encodage
-=======
-			//FileWriter n'accepte pas un 2Ã¨me argument pour spÃ©cifier l'encodage
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
-			
+			// FileWriter n'accepte pas un 2ème argument pour spécifierl'encodage
+
 			// instancier un objet de la classe URL
 			URL url = new URL(urlstart);
-            
-			// imprimer cet objet
-<<<<<<< HEAD
-			System.out.println("URL à aspirer ==>" + url);
-=======
-			System.out.println("URL Ã  aspirer ==>" + url);
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
 
-			
+			// imprimer cet objet
+			System.out.println("URL à aspirer ==>" + url);
+
 			BufferedReader lire = new BufferedReader(new InputStreamReader(url.openStream()));
 
 			do
 			// faire ce qui suit
 			{
+				progressBar.setValue(c);
+				label2.setText("URL " + c);
 				String line = lire.readLine();
 
 				System.out.println(line);
@@ -86,67 +146,47 @@ public class ExtraInfo {
 				}
 
 				ecrire.write(line);
-				
-<<<<<<< HEAD
+
 				// générer dans sauts de ligne
-=======
-				// gÃ©nÃ©rer dans sauts de ligne
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
 				ecrire.write(System.getProperty("line.separator"));
 
-			// tant que c'est vrai (true)	
+				// tant que c'est vrai (true)
 			} while (true);
-			
-<<<<<<< HEAD
-            // fermer le flux d'écriture
-=======
-            // fermer le flux d'Ã©criture
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
-			ecrire.close();
-			
-			//concatenation 
-			ecrire = new PrintWriter(new FileOutputStream("subst", true));
-		}
-		
 
-		 //////////////////////////////////////////////////////////////////////
-<<<<<<< HEAD
-		/////////////// Les Médicaments par Nom Commercial///////////////////
-=======
-		/////////////// Les MÃ©dicaments par Nom Commercial///////////////////
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
-		//////////////////////////////////////////////////////////////////////
+			// fermer le flux d'écriture
+			ecrire.close();
+
+			// concatenation
+			ecrire = new PrintWriter(new FileOutputStream("subst", true));
+
+			/* ========barre de progresion========= */
+			progressBar.setValue(0);
+		}
+
+		// ////////////////////////////////////////////////////////////////////
+		// ///////////// Les Médicaments par Nom Commercial///////////////////
+		// ////////////////////////////////////////////////////////////////////
 
 		ecrire2 = new PrintWriter("medic");
-		
+
 		for (c = 'A'; c <= 'Z'; c++) {
 
-			// URL url = new URL("http://www.vidal.fr/Sommaires/Medicaments-A.htm");
+			// URL url = new
+			// URL("http://www.vidal.fr/Sommaires/Medicaments-A.htm");
 			// Ou bien...
 			urlstart = "http://www.vidal.fr/Sommaires/Medicaments-" + c+ ".htm";
 
-<<<<<<< HEAD
-			// spécifier l'URL (plus tard), elle sera insérée dans une boîte de saisie de type InputDialog
+			// spécifier l'URL (plus tard), elle sera insérée dans une boîte de
+			// saisie de type InputDialog
 
 			// Ou bien PrintWriter ecrire =new PrintWriter (new FileWriter("subst.dic")) ;
-			//FileWriter n'accepte pas un 2ème argument pour spécifier l'encodage
-=======
-			// spÃ©cifier l'URL (plus tard), elle sera insÃ©rÃ©e dans une boÃ®te de saisie de type InputDialog
+			// FileWriter n'accepte pas un 2ème argument pour spécifier l'encodage
 
-			// Ou bien PrintWriter ecrire =new PrintWriter (new FileWriter("subst.dic")) ;
-			//FileWriter n'accepte pas un 2Ã¨me argument pour spÃ©cifier l'encodage
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
-			
 			// instancier un objet de la classe URL
 			URL url = new URL(urlstart);
 
 			// imprimer cet objet
-<<<<<<< HEAD
 			System.out.println("URL à aspirer ==>" + url);
-=======
-			System.out.println("URL Ã  aspirer ==>" + url);
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
-			
 
 			BufferedReader lire = new BufferedReader(new InputStreamReader(url.openStream()));
 
@@ -161,27 +201,24 @@ public class ExtraInfo {
 				}
 
 				ecrire2.write(line);
-				
-<<<<<<< HEAD
+
 				// générer dans sauts de ligne
-=======
-				// gÃ©nÃ©rer dans sauts de ligne
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
 				ecrire2.write(System.getProperty("line.separator"));
-				
-             // tant que c'est vrai (true)
+
+				// tant que c'est vrai (true)
 			} while (true);
-			
-<<<<<<< HEAD
+
 			// fermer le flux d'écriture
-=======
-			// fermer le flux d'Ã©criture
->>>>>>> bf405a7602303ffd4882a4ec96af537b78098f08
 			ecrire2.close();
-			
-			//concatenation 
+
+			// concatenation
 			ecrire2 = new PrintWriter(new FileOutputStream("medic", true));
 		}
-		
+
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+
 	}
 }
